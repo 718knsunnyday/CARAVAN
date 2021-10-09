b854927e7247a53bd4eb17935895ad1e3995dbd1
718knsunnyday <718mickeeey@gmail.com>
2021-10-09 13:56:32 +0000

    7章

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,16 +22,26 @@ class BlogsController < ApplicationController
   end
   
   def edit
+    @blog=Blog.find(params[:id])
+    
   end
   
   def update
+    blog=Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blogs_path(blog)
   end
   
   def destroy
+    blog=Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
   
+  private
+  
   def blog_params
-    params.require(:blog).permit(:title,:category,:body)
+    params.require(:blog).permit(:title, :category, :body)
   end
 
 end
